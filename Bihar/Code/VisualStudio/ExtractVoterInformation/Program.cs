dea47aa8f8921a192ea1204ffefb262375f37988
panuganti <rajkiran.panuganti@gmail.com>
2015-07-14 23:00:35 -0700

    updates from laptop

---
+++ Bihar/Code/VisualStudio/ExtractVoterInformation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExtractVoterInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"C:\Users\rapanuga\Desktop\ElectoralRoll_1_1.txt";
+            var lines = File.ReadAllLines(filename, Encoding.Unicode);
+            Console.Write(lines);
+        }
+    }
+}
