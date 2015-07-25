f728fc5e9460fabd92e47da3ab5e5435fd806d05
rapanuga <rapanuga@microsoft.com>
2015-07-24 18:16:48 -0700

    Changes from Work

---
+++ Bihar/Code/VisualStudio/BiharViz/BiharMap.ts
@@ -0,0 +1,34 @@
+﻿/// <reference path="./MapStyles.ts"/>
+/// <reference path="./Scripts/typings/googlemaps/google.maps.d.ts"/>
+
+module BiharElections {
+    export class BiharMap {
+        private mapDiv: HTMLElement;
+        private map: google.maps.Map;
+        private mapOptions: google.maps.MapOptions;
+
+        constructor(mapDiv: HTMLElement) {
+            this.mapDiv = mapDiv;
+            this.initialize();
+        }
+
+        initialize() {
+            this.mapOptions = {
+                zoom: 5,
+                center: new google.maps.LatLng(23, 84),
+                mapTypeId: google.maps.MapTypeId.TERRAIN,
+                minZoom: 5,
+                disableDefaultUI: true
+            };
+            this.map = new google.maps.Map(this.mapDiv, this.mapOptions);
+        }
+
+        setMapStyles() {
+            var mapType = new ArchishaMapStyle();
+            this.map.mapTypes.set(mapType.name, mapType.archishaMapType);
+            this.mapOptions.mapTypeId = mapType.name;
+            this.mapOptions.mapTypeControlOptions = { mapTypeIds: [mapType.name, google.maps.MapTypeId.TERRAIN] };
+        }
+    }
+}
+
---
+++ Bihar/Code/VisualStudio/BiharViz/MapStyles.ts
@@ -0,0 +1,46 @@
+﻿/// <reference path="./Scripts/typings/googlemaps/google.maps.d.ts"/>
+/// <reference path="./BiharMap.ts"/>
+
+module BiharElections {
+
+    export class ArchishaMapStyle {
+        archishaMapType : google.maps.StyledMapType;
+        name: any;
+
+        constructor() {
+            this.createMapType();
+            this.name = 'ArchishaStyle';
+        }
+        createMapType() {
+            var featureOpts: google.maps.MapTypeStyle[] = [
+                {
+                    stylers: [
+                        { hue: '#890000' },
+                        { visibility: 'simplified' },
+                        { gamma: 0.5 },
+                        { weight: 0.5 }
+                    ]
+                },
+                {
+                    elementType: 'labels',
+                    stylers: [
+                        { visibility: 'off' }
+                    ]
+                },
+                {
+                    featureType: 'water',
+                    stylers: [
+                        { color: '#890000' }
+                    ]
+                }
+            ];
+
+            var styledMapOptions: google.maps.StyledMapTypeOptions = {
+                name: 'Archisha Style'
+            };
+
+            this.archishaMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+        }
+    }
+}
+
