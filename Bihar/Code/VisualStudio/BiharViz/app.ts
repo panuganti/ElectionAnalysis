f655ab54cb42b895a8ca00f5492e7c4b4a18dd39
rapanuga <rapanuga@microsoft.com>
2015-07-24 17:25:23 -0700

    Add TS changes from Work

---
+++ Bihar/Code/VisualStudio/BiharViz/app.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="./Scripts/typings/googlemaps/google.maps.d.ts"/>
+
+module BiharElections {
+    export class BiharMap {
+        private mapDiv: HTMLElement;
+
+        constructor(mapDiv: HTMLElement) {
+            this.mapDiv = mapDiv;
+            this.initialize();
+        }
+
+        initialize() {
+            var map;
+            var mapOptions: google.maps.MapOptions = {
+                zoom: 5,
+                center: new google.maps.LatLng(23, 84),
+                mapTypeId: google.maps.MapTypeId.TERRAIN,
+                minZoom: 5,
+                disableDefaultUI: true
+            };
+            map = new google.maps.Map(this.mapDiv, mapOptions);
+        }
+    }
+}
+
+function initialize() {
+    var map = new BiharElections.BiharMap(document.getElementById('map-canvas'));
+}
+
+//google.maps.event.addDomListener(window, 'load', initialize);
+
+
+window.onload = () => {
+    var map = new BiharElections.BiharMap(document.getElementById('map-canvas'));
+};
+
+
