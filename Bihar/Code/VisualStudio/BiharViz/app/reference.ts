fe758cd245ef7d518a1e40beaf6ff7e889741c01
rapanuga <rapanuga@microsoft.com>
2015-08-01 18:27:28 -0700

    Updates from Work Desktop

---
+++ Bihar/Code/VisualStudio/BiharViz/app/reference.ts
@@ -0,0 +1,8 @@
+//grunt-start
+/// <reference path="BiharMap.ts" />
+/// <reference path="DataLoader.ts" />
+/// <reference path="EventHandler.ts" />
+/// <reference path="MapStyles.ts" />
+/// <reference path="app.ts" />
+/// <reference path="topojson.d.ts" />
+//grunt-end
