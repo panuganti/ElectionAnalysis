45ab178631fa874c89453bd189af3242dcab3aa7
panuganti <rajkiran.panuganti@gmail.com>
2015-08-06 21:45:02 -0700

    Update gitignore

---
+++ Bihar/Code/VisualStudio/BiharElectionsLibrary/Colors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BiharElectionsLibrary
+{
+    [DataContract]
+    public class Colors
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+
+        public static List<Colors> LoadColors()
+        {
+            var lines = File.ReadAllLines("./Legend.txt");
+            return lines.Select(t => 
+            {
+                var parts = t.Split('\t'); return new Colors { Name = parts[0], Color = parts[1] };
+            }).ToList();
+        }
+    }
+}
