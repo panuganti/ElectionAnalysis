cae523c0c6184a4dca85e7e787c1dca314e18d7e
panuganti <rajkiran.panuganti@gmail.com>
2015-09-01 00:09:54 -0700

    Updates from laptop; Parse CVoter html pages

---
+++ India/BiharElectionsLibrary/Utils/TableExtractor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace Utils
+{
+    class TableExtractor
+    {
+        public static Table ExtractTableFromDiv(HtmlNode node, bool hasHeader = true)
+        { // We assume that the input node is a table node
+
+            var table = new Table();
+            if (hasHeader)
+            {
+                table.Headers = node.Descendants("th").Select(t => t.InnerText).ToList();
+            }
+
+            var rowNodes = node.Descendants("tbody").First().ChildNodes;
+            table.Rows = new List<List<string>>();
+            foreach (var rowNode in rowNodes)
+            {
+                if (rowNode.Name == "tr")
+                {
+                    table.Rows.Add(rowNode.Descendants("td").Select(t => t.InnerText).ToList());
+                }
+            }
+            return table;
+        }
+    }
+
+    class Table
+    {
+        public List<string> Headers { get; set; }
+
+        public List<List<string>> Rows { get; set; }
+    }
+}
--- India/CVoterLibrary/Qualitative/DataLoader.cs
+++ India/CVoterLibrary/Qualitative/DataLoader.cs
@@ -3,37 +3,45 @@ using System.IO;
 using System.Linq;
 using HtmlAgilityPack;
 using CVoterContracts;
+using Utils;
+using System;
 
 namespace CVoterLibrary
 {
     public class DataLoader
     {
-        public static QualitativeData LoadDataFromDir(string dirPath)
+        public static Tuple<QualitativeData,QualitativeData> LoadDataFromDir(string dirPath)
         {
-            var data = new QualitativeData {AcQualitatives = new List<AcQualitative>()};
+            var data1 = new QualitativeData { AcQualitatives = new List<AcQualitative>() };
+            var data2 = new QualitativeData { AcQualitatives = new List<AcQualitative>() };
+
             var allFiles = Directory.GetFiles(dirPath);
             foreach (var file in allFiles)
             {
-                data.AcQualitatives.Add(LoadDataFromFile(file));
+                var tuple = LoadDataFromFile(file);
+                data1.AcQualitatives.Add(tuple.Item1);
+                data2.AcQualitatives.Add(tuple.Item2);
             }
-            return data;
+            return new Tuple<QualitativeData,QualitativeData>(data1, data2);
         }
 
-        public static AcQualitative LoadDataFromFile(string filename)
+        public static Tuple<AcQualitative,AcQualitative> LoadDataFromFile(string filename)
         {
-            var acData = new AcQualitative();
+            var acData1 = new AcQualitative();
+            var acData2 = new AcQualitative();
             var doc = new HtmlWeb().Load(filename);
             // TODO: Instead, just look for all the option nodes
             var q1Div = doc.DocumentNode.Descendants("div").First(t => t.Attributes.Contains("class") && t.Attributes["class"].Value == "q1"); 
             // Read dictionary of id, name
             var acDict = GetAcDictionary(q1Div);
             var acName = Path.GetFileNameWithoutExtension(filename);
-            acData.Name = acDict.Keys.First(t => t == acName);
-            acData.No = acDict[acData.Name]; // TODO: Use relaxed matching
-            
+            acData1.Name = acDict.Keys.First(t => t == acName);
+            acData2.Name = acData1.Name;
+            acData1.No = acDict[acData1.Name]; // TODO: Use relaxed matching
+            acData2.No = acData1.No;
             // Read each table of the file
-
-            return acData; // Two AcDatas are returned
+            ParsePage(doc.DocumentNode, acData1, acData2);
+            return new Tuple<AcQualitative,AcQualitative>(acData1,acData2); // Two AcDatas are returned
         }
 
         public static Dictionary<string, int> GetAcDictionary(HtmlNode qDiv)
@@ -51,41 +59,81 @@ namespace CVoterLibrary
             var tableDivs = rootNode.Descendants("div")
                 .Where(t => t.Attributes.Contains("class") && t.Attributes["class"].Value == "divgrds").ToArray();
             // First two are local issues
-            ParseLocalIssuesData(tableDivs[0]);
-            ParseLocalIssuesData(tableDivs[1]);
+            acData1.LocalIssues = ParseLocalIssuesData(tableDivs[0]);
+            acData2.LocalIssues = ParseLocalIssuesData(tableDivs[1]);
             // Next two are dev params
-            ParseDevParamsData(tableDivs[2]);
-            ParseDevParamsData(tableDivs[3]);
+            acData1.DevParams = ParseDevParamsData(tableDivs[2]);
+            acData2.DevParams = ParseDevParamsData(tableDivs[3]);
             // Next two are cand params
-            ParseCandidateParamsData(tableDivs[4]);
-            ParseCandidateParamsData(tableDivs[5]);
+            acData1.CandidateRatings = ParseCandidateParamsData(tableDivs[4]);
+            acData2.CandidateRatings = ParseCandidateParamsData(tableDivs[5]);
             // next two are party params
-            ParsePartyParamsData(tableDivs[6]);
-            ParsePartyParamsData(tableDivs[7]);
+            acData1.PartyRatings = ParsePartyParamsData(tableDivs[6]);
+            acData2.PartyRatings = ParsePartyParamsData(tableDivs[7]);
             // next two are caste params
-            ParseCasteShareData(tableDivs[8]);
-            ParseCasteShareData(tableDivs[9]);
+            acData1.CasteShares = ParseCasteShareData(tableDivs[8]);
+            acData2.CasteShares = ParseCasteShareData(tableDivs[9]);
         }
 
-        public static void ParseLocalIssuesData(HtmlNode localIssuesNode)
-        {            
-        }
-
-        public static void ParseDevParamsData(HtmlNode localIssuesNode)
+        public static List<Rating> ParseLocalIssuesData(HtmlNode localIssuesNode)
         {
+            var ratings = new List<Rating>();
+            var table = TableExtractor.ExtractTableFromDiv(localIssuesNode);
+            foreach (var row in table.Rows)
+            {
+                ratings.Add(new Rating { Feature = row[0], Score = int.Parse(row[1])});
+            }
+            return ratings;
         }
 
-        public static void ParseCandidateParamsData(HtmlNode localIssuesNode)
+        public static List<Rating> ParseDevParamsData(HtmlNode devParamsNode)
         {
+            var ratings = new List<Rating>();
+            var table = TableExtractor.ExtractTableFromDiv(devParamsNode);
+            foreach (var row in table.Rows)
+            {
+                ratings.Add(new Rating { Feature = row[0], Score = int.Parse(row[1]) });
+            }
+            return ratings;
         }
 
-        public static void ParsePartyParamsData(HtmlNode localIssuesNode)
+        public static List<CandidateRating> ParseCandidateParamsData(HtmlNode candidateParamsNode)
         {
+            var ratings = new List<CandidateRating>();
+            var table = TableExtractor.ExtractTableFromDiv(candidateParamsNode);
+            var headers = table.Headers.Skip(2).ToArray();
+            foreach (var row in table.Rows)
+            {
+                var candRating = new CandidateRating { Name = row[1], PartyName = row[0], Ratings = new List<Rating>() };
+                for (int i = 0; i < headers.Length; i++ )
+                {
+                    candRating.Ratings.Add(new Rating { Feature = headers[i], Score = int.Parse(row[i + 2]) });
+                }
+                    ratings.Add(candRating);
+            }
+            return ratings;
         }
 
-        public static void ParseCasteShareData(HtmlNode localIssuesNode)
+        public static List<PartyRating> ParsePartyParamsData(HtmlNode partyParamsNode)
         {
+            var ratings = new List<PartyRating>();
+            var table = TableExtractor.ExtractTableFromDiv(partyParamsNode);
+            var headers = table.Headers.Skip(1).ToArray();
+            foreach (var row in table.Rows)
+            {
+                var partyRating = new PartyRating { PartyName = row[0], Ratings = new List<Rating>() };
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    partyRating.Ratings.Add(new Rating { Feature = headers[i], Score = int.Parse(row[i + 1]) });
+                }
+                ratings.Add(partyRating);
+            }
+            return ratings;
         }
 
+        public static List<CasteShare> ParseCasteShareData(HtmlNode casteSharesNode)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
