77cd49b6c579a8a59ef759d976a295ae4c9a35c6
rapanuga <rapanuga@microsoft.com>
2015-09-11 17:33:27 -0700

    fix break

---
+++ ElectionsSPA/app/models/InfoData.ts
@@ -0,0 +1,7 @@
+/// <reference path="../reference.ts" />
+module Models {
+	export class InfoData {
+        constructor(public Title: string, public Results2009: Models.Result, public Results2010: Models.Result, public Results2014: Models.Result)
+        { }
+    }
+}
