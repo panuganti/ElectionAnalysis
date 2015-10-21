219e47e3c6aae0b49eb60b2c2b3ceb1a90d856c6
rapanuga <rapanuga@microsoft.com>
2015-10-20 18:40:01 -0700

    Updates from Work

--- India/ExtractFeatures/Program.cs
+++ India/ExtractFeatures/Program.cs
@@ -13,27 +13,59 @@ namespace ExtractFeatures
     {
         static void Main(string[] args)
         {
+            ExtractFeatures();
         }
 
         private static void ExtractFeatures()
         {
-            string rootDir = File.ReadAllLines(@".\Config.ini").First();
-            string stateDivisionsFilename = Path.Combine(rootDir, ConfigurationManager.AppSettings["StateDivisionsFilename"]);
-            string acInfoFilename = Path.Combine(rootDir, ConfigurationManager.AppSettings["ACs"]);
+            string startingExtraction = @"I:/ArchishaData/ElectionData/Bihar/Predictions/Extraction2010.tsv";
+            string candParamsFile = @"I:/ArchishaData/ElectionData/Bihar/CVoterData/2010/Qualitative/CombinedQualitativeData/CandParams.txt";
+            string outExtraction = @"I:/ArchishaData/ElectionData/Bihar/Predictions/Extraction2010Out.tsv";
+            var extractionHeader = File.ReadAllLines(startingExtraction).First().Split('\t');
+            var extraction = File.ReadAllLines(startingExtraction).Skip(1).Select(t =>
+            {
+                var parts = t.Split('\t');
+                return
+                    new
+                    {
+                        AcNo = parts[0],
+                        CandidateName = parts[1],
+                        Party = parts[2],
+                        Result = parts[3],
+                        Params = String.Join("\t",parts.Skip(4))
+                    };
+            }).ToArray();
+            var candParamsHeader = File.ReadAllLines(candParamsFile).First().Split('\t');
+            var candParams = File.ReadAllLines(candParamsFile).Skip(1).Select(t =>
+            {
+                var parts = t.Split('\t');
+                return
+                    new
+                    {
+                        acId = parts[0],
+                        Candidate = parts[1],
+                        Party = parts[2],
+                        Params = String.Join("\t",parts.Skip(3))
+                    };
+            }).ToArray();
 
-            string indiaVotesResults2014Dir = Path.Combine(rootDir, ConfigurationManager.AppSettings["2014ACWiseResultsIndiaVotes"]);
-            string indiaVotesResults2010Dir = Path.Combine(rootDir, ConfigurationManager.AppSettings["2010ACWiseResultsIndiaVotes"]);
-            string indiaVotesResults2009Dir = Path.Combine(rootDir, ConfigurationManager.AppSettings["2009ACWiseResultsIndiaVotes"]);
-
-            State state = PopulateInfo.LoadElectionHierarchy(stateDivisionsFilename, acInfoFilename);
-
-            var indiaVotesResults2014 = ResultsLoader.LoadResultsFromIndiaVotesData(indiaVotesResults2014Dir, 2014);
-            var indiaVotesResults2009 = ResultsLoader.LoadResultsFromIndiaVotesData(indiaVotesResults2009Dir, 2009);
-            var indiaVotesResults2010 = ResultsLoader.LoadACResultsFromIndiaVotesData(indiaVotesResults2010Dir, 2010);
-            List<Result> conflatedResults2009 = ResultsConflator.ConflateResults(indiaVotesResults2009, state);
-            List<Result> conflatedResults2010 = ResultsConflator.ConflateResultsAndDistrictInfo(indiaVotesResults2010, state);
-            List<Result> conflatedResults2014 = ResultsConflator.ConflateResults(indiaVotesResults2014, state);            
-
+            var newExtraction = new List<string>();
+            var header = String.Format("{0}\t{1}\t{2}\n", "AcNo\tCandidateName\tParty\tResult",String.Join("\t",extractionHeader.Skip(4)), String.Join("\t", candParamsHeader.Skip(3)));
+            foreach (var featureVector in extraction)
+            {
+                var possibleCandidates = candParams.Where(t => t.Candidate.ToLower() == featureVector.CandidateName.ToLower() && featureVector.AcNo == t.acId).ToArray();
+                if (possibleCandidates.Length == 1)
+                {
+                    var cand = possibleCandidates.First();
+                    newExtraction.Add(String.Join("\t", new[] { featureVector.AcNo, featureVector.CandidateName, featureVector.Party, featureVector.Result, featureVector.Params, String.Join("\t", cand.Params) }));
+                }
+                else // Assume no suitable candidate was available
+                {
+                    newExtraction.Add(String.Join("\t", new string[] { featureVector.AcNo, featureVector.CandidateName, featureVector.Party, featureVector.Result, featureVector.Params, String.Join("\t", candParamsHeader.Skip(3).Select(t=>"")) }));
+                }
+            }
+            File.WriteAllText(outExtraction,header);
+            File.AppendAllLines(outExtraction,newExtraction);
         }
     }
 }
--- India/ReadHtmlQuantitativeData/Program.cs
+++ India/ReadHtmlQuantitativeData/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using HtmlAgilityPack;
 
 namespace ReadHtmlQuantitativeData
 {
@@ -9,27 +7,12 @@ namespace ReadHtmlQuantitativeData
     {
         static void Main(string[] args)
         {
-            ParseHtmlFiles();
-        }
-
-        private static void ParseHtmlFiles()
-        {
-            string inPath = @"I:\ArchishaData\ElectionData\RawData\CVoter\QualitativeData";
-            string outPath = @"I:\ArchishaData\ElectionData\Bihar\CVoterData\2010\Qualitative";
-            for (int i = 1; i <= 243; i++)
-            {
-                var inFile = Path.Combine(inPath, String.Format("{0}.html",i));
-                //var parsedData = ParseHtmlData.ParseHtml(inFile);
-                Console.ReadKey();
-            }             
-        }
-
-        private static void ParseManuallyStoredHtmlFiles()
-        {
-            const string filesPath = @"I:\ArchishaData\ElectionData\RawData\CVoter\QualitativeData";
-            var htmlFiles = Directory.GetFiles(filesPath).Where(t => t.EndsWith(".html"));
+            const string filesPath = @"E:\NMW\SurveyAnalytics\Bihar\CVoterData\2015QualitativeData";
+            var htmlFiles = Directory.GetFiles(filesPath).Where(t=>t.EndsWith(".html"));
 
             var dict = htmlFiles.ToDictionary(Path.GetFileNameWithoutExtension, ParseHtmlData.ParseHtml);
+
+
         }
     }
 }
