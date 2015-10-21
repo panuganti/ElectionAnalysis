4183fb2c3e5c2587b5d326bcee43ef80a44fbd02
panuganti <rajkiran.panuganti@gmail.com>
2015-10-21 09:01:42 -0700

    Fix bug by ensuring Distinct Handles for judging

---
+++ US/RemoveDuplicateProfiles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicateProfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllLines(@"C:\Projects\ElectionAnalysis\US\Judging\allHandlesDistinct.txt", File.ReadAllLines(@"C:\Projects\ElectionAnalysis\US\Judging\allHandles.txt").Distinct());
+        }
+    }
+}
