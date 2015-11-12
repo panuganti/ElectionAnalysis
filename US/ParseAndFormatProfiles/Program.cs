e452465bc5be633b693c2046ffb785cc517b2f68
rapanuga <rapanuga@microsoft.com>
2015-11-11 17:11:39 -0800

    Parse Tweets

--- US/ParseAndFormatProfiles/Program.cs
+++ US/ParseAndFormatProfiles/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Policy;
+using System.Text;
 using System.Text.RegularExpressions;
+using Google.GData.Client;
 using HtmlAgilityPack;
 
 namespace ParseAndFormatProfiles
@@ -12,13 +15,13 @@ namespace ParseAndFormatProfiles
     {
         static void Main(string[] args)
         {
-            FormatTweets();
+            ParseTweets();
         }
 
         static void FormatTweets()
         {
-            const string tweetsDir = @"D:\ArchishaData\ElectionData\US\Tweets";
-            const string outfile = @"D:\ArchishaData\ElectionData\US\FormattedTweets.tsv";
+            const string tweetsDir = @"I:\ArchishaData\ElectionData\US\Tweets";
+            const string outfile = @"I:\ArchishaData\ElectionData\US\FormattedTweets.tsv";
             var files = Directory.GetFiles(tweetsDir);
             File.WriteAllLines(outfile,files.SelectMany(x =>
             {
@@ -30,21 +33,21 @@ namespace ParseAndFormatProfiles
 
         static void ParseJudgements()
         {
-            const string judgementsFile = @"D:\ArchishaData\ElectionData\US\Judgements.tsv";
-            const string outfile = @"D:\ArchishaData\ElectionData\US\JudgementsParsed.tsv";
+            const string judgementsFile = @"I:\ArchishaData\ElectionData\US\Judgements.tsv";
+            const string outfile = @"I:\ArchishaData\ElectionData\US\JudgementsParsed.tsv";
             File.WriteAllLines(outfile, File.ReadAllLines(judgementsFile).SelectMany(x =>
             {
                 var parts = x.Split('\t');
                 var judgements = parts[4].Split(';').Skip(1);
                 int count = 0;
-                return judgements.Select(y => { count++; return String.Format("{0}__{1}\t{2}", parts[1], count, y); });
+                return judgements.Select(y => { count++; return String.Format("{0}__{1}\t{2}\t{3}", parts[1], count, parts[0], y); });
             }));
         }
 
         static void ParseTweets()
         {
-            const string republicanProfilesDir = @"D:\ArchishaData\ElectionData\US\RepublicanProfiles";
-            const string democraticProfilesDir = @"D:\ArchishaData\ElectionData\US\DemocraticProfiles";
+            const string republicanProfilesDir = @"I:\ArchishaData\ElectionData\US\RepublicanProfiles";
+            const string democraticProfilesDir = @"I:\ArchishaData\ElectionData\US\DemocraticProfiles";
             const string outputDir = @"D:\ArchishaData\ElectionData\US\Tweets\";
             ParseProfilesForTweets(republicanProfilesDir,outputDir);
             ParseProfilesForTweets(democraticProfilesDir, outputDir);
@@ -139,7 +142,7 @@ namespace ParseAndFormatProfiles
                     var htmlDoc = new HtmlDocument { OptionFixNestedTags = true };
                     htmlDoc.Load(file);
                     var timelineNode = htmlDoc.GetElementbyId("timeline");
-                    File.WriteAllLines(outfile, ExtractTweets(timelineNode));
+                    File.WriteAllLines(outfile, ExtractTweets(timelineNode).Where(x=>!x.Text.Equals(String.Empty)).Select(x=>String.Format("{0}",x.Text)));
                     count++; Console.WriteLine("count: {0}:", count);
                 }
                 catch (Exception)
@@ -148,16 +151,57 @@ namespace ParseAndFormatProfiles
             }
         }
 
-        private static IEnumerable<string> ExtractTweets(HtmlNode node)
+        [DataContract]
+        public class Tweet
+        {
+            [DataMember]
+            public string Text { get; set; }
+            [DataMember]
+            public IEnumerable<string> Images { get; set; }
+            [DataMember]
+            public IEnumerable<string> Links { get; set; }
+            [DataMember]
+            public IEnumerable<string> Mentions { get; set; }
+            [DataMember]
+            public IEnumerable<string> HashTags { get; set; }
+            [DataMember]
+            public int RTs { get; set; }
+            [DataMember]
+            public int Favs { get; set; }
+        }
+
+        private static IEnumerable<Tweet> ExtractTweets(HtmlNode node)
         {
             var tweetNodes = node.Descendants("li")
                 .Where(t => t.Attributes.Contains("class") && t.Attributes["class"].Value.Contains(
                     "js-stream-item stream-item stream-item expanding-stream-item"));
-            var tweets = new List<string>();
+            var tweets = new List<Tweet>();
+            var mentionPattern = "@([a-zA-Z0-9_]+)";
+            var hashTagPattern = "#([a-zA-Z0-9_]+)";
+            var urlPattern = @"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*))";
+            var imagePattern = @"(pic.twitter.com\/[a-zA-Z0-9]+)";
             foreach (var tweetNode in tweetNodes)
             {
-                var text = String.Join(" ", tweetNode.Descendants("div").Where(x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "content").First().Descendants("p").First().InnerText.Replace('\n', ' ').Split(' ').Select(x => x.Trim()).Where(x => x.Length > 0));
-                tweets.Add(text);
+                var tweetText =
+                    tweetNode.Descendants("div")
+                        .First(x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "content")
+                        .Descendants("p")
+                        .First()
+                        .InnerText.Replace('\n', ' ');
+                // TODO: Extract mentions, pics, links, correct html encodes, popularity info.
+                tweetText = HttpUtility.HtmlDecode(tweetText);
+                var mentions = Regex.Matches(tweetText, mentionPattern).Cast<Match>().Select(x=>x.Value);
+                var hashtags = Regex.Matches(tweetText, hashTagPattern).Cast<Match>().Select(x => x.Value);
+                tweetText = Regex.Replace(tweetText, hashTagPattern, String.Empty);
+                var urls = Regex.Matches(tweetText, urlPattern).Cast<Match>().Select(x => x.Value);
+                tweetText = Regex.Replace(tweetText, urlPattern, String.Empty);
+                var images = Regex.Matches(tweetText, imagePattern).Cast<Match>().Select(x => x.Value);
+                tweetText = Regex.Replace(tweetText, imagePattern, String.Empty);
+                tweetText = Encoding.ASCII.GetString(Encoding.Convert(Encoding.UTF8,
+                        Encoding.GetEncoding(Encoding.ASCII.EncodingName, new EncoderReplacementFallback(string.Empty),
+                            new DecoderExceptionFallback()), Encoding.UTF8.GetBytes(tweetText)));
+                var text = String.Join(" ", tweetText.Split(' ').Select(x => x.Trim()).Where(x => x.Length > 0));
+                tweets.Add(new Tweet { Text = text, Mentions = mentions, Images = images, Links = urls, HashTags = hashtags});
             }
             return tweets;
         }
--- US/R/PrepareHRS.R
+++ US/R/PrepareHRS.R
@@ -1,10 +1,12 @@
-tweets <- read.delim("D:/ArchishaData/ElectionData/US/FormattedTweets.tsv", header=FALSE)
+tweets <- read.delim("I:/ArchishaData/ElectionData/US/FormattedTweets.tsv", header=FALSE)
 names(tweets)[1] = "TweetId"
 names(tweets)[2] = "Tweet"
-judgements <- read.delim("D:/ArchishaData/ElectionData/US/JudgementsParsed.tsv", header=FALSE)
+judgements <- read.delim("I:/ArchishaData/ElectionData/US/JudgementsParsed.tsv", header=FALSE)
 names(judgements)[1] = "TweetId"
-names(judgements)[2] = "Judgement"
+names(judgements)[2] = "Judge"
+names(judgements)[3] = "Judgement"
 hrs = merge(tweets,judgements,by="TweetId",all.x=TRUE);
 hrs = merge(tweets,judgements,by="TweetId",all.x=TRUE);
 hrs_train = merge(tweets,judgements,by="TweetId");
 hrs_train_unique = unique(hrs_train)
+write.table(hrs_train_unique[,c(3,4,2,1)],"I:/ArchishaData/ElectionData/US/TrainingSet.tsv",quote=TRUE,sep="\t",row.names=FALSE,col.names=FALSE)
