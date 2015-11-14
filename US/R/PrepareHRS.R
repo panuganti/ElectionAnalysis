e23e9f0a46511d36941a8916303a667348c3a598
rapanuga <rapanuga@microsoft.com>
2015-11-13 18:21:01 -0800

    Improve accuracy

--- US/R/PrepareHRS.R
+++ US/R/PrepareHRS.R
@@ -1,12 +1,13 @@
 tweets <- read.delim("I:/ArchishaData/ElectionData/US/FormattedTweets.tsv", header=FALSE)
-names(tweets)[1] = "TweetId"
-names(tweets)[2] = "Tweet"
+names(tweets) = c("TweetId","Tweet","NoOfImages","AnyHashtags","AnyLinks","NoOfMentions","Hashtags","HashtagWithDemCands","HashtagWithRepCands","TweetWithDemCands","TweetWithRepCands")
 judgements <- read.delim("I:/ArchishaData/ElectionData/US/JudgementsParsed.tsv", header=FALSE)
-names(judgements)[1] = "TweetId"
-names(judgements)[2] = "Judge"
-names(judgements)[3] = "Judgement"
+names(judgements) = c("TweetId","Judge","Judgement");
 hrs = merge(tweets,judgements,by="TweetId",all.x=TRUE);
 hrs = merge(tweets,judgements,by="TweetId",all.x=TRUE);
 hrs_train = merge(tweets,judgements,by="TweetId");
 hrs_train_unique = unique(hrs_train)
-write.table(hrs_train_unique[,c(3,4,2,1)],"I:/ArchishaData/ElectionData/US/TrainingSet.tsv",quote=TRUE,sep="\t",row.names=FALSE,col.names=FALSE)
+hrs_train_unique[hrs_train_unique$Judgement=="AntiDemocratic",] = "ProRepublican"
+write.table(hrs_train_unique[,c(13,2,3,4,5,6,7,8,9,10,11)],"I:/ArchishaData/ElectionData/US/TrainingSet.tsv",quote=TRUE,sep=",",row.names=FALSE,col.names=FALSE)
+write.table(hrs_train_unique[,c(12,1,13,2,3,4,5,6,7,8,9,10,11)],"I:/ArchishaData/ElectionData/US/JudgedTrainingSet.tsv",quote=TRUE,sep="\t",row.names=FALSE,col.names=TRUE)
+
+write.table(tset,"I:/ArchishaData/ElectionData/US/tSet.tsv",quote=TRUE,sep=",",row.names=FALSE,col.names=FALSE)
